92cae0448e11e961c58f126c1e406e50d75442d3
aManOfCultr <amancloud882@gmail.com>
2024-11-02 16:55:18 +0530

    Store picked image locally using Path Provider

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,17 +1,24 @@
 import 'dart:io';
-
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:mapmate/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title, File image, PlaceLocation location) {
-    final newPlace = Place(title: title, image: image,location: location);
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$fileName');
+
+    final newPlace =
+        Place(title: title, image: copiedImage, location: location);
     state = [newPlace, ...state];
   }
 }
 
-final userPlacesProvider = StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+final userPlacesProvider =
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
   (ref) => UserPlacesNotifier(),
 );
