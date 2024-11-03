83b5f1e9a009e1354e0414ca98b647767d8b45a6
aManOfCultr <amancloud882@gmail.com>
2024-11-03 20:19:43 +0530

    Store and retrive place metadata from local sql database

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -4,35 +4,55 @@ import 'package:mapmate/providers/user_places.dart';
 import 'package:mapmate/screens/add_place.dart';
 import 'package:mapmate/widgets/places_list.dart';
 
-class PlacesScreen extends ConsumerWidget {
+class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return _PlaceScreenState();
+  }
+}
+
+class _PlaceScreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final userPlaces = ref.watch(userPlacesProvider);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Places'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                builder: (ctx) => const AddPlaceScreen(),
-              ));
-            },
-            icon: const Icon(Icons.add_location_alt_outlined),
+        appBar: AppBar(
+          title: const Text('Your Places'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (ctx) => const AddPlaceScreen(),
+                ));
+              },
+              icon: const Icon(Icons.add_location_alt_outlined),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 8.0,
           ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 8.0,
-        ),
-        child: PlacesList(
-          places: userPlaces,
-        ),
-      ),
-    );
+          child: FutureBuilder(
+            future: _placesFuture,
+            builder: (context, snapshot) =>
+                snapshot.connectionState == ConnectionState.waiting
+                    ? const Center(child: CircularProgressIndicator())
+                    : PlacesList(
+                        places: userPlaces,
+                      ),
+          ),
+        ));
   }
 }
